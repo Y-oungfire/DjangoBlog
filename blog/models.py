230b5f0dda6293bd436d1cfa9aefa90118a2d518
Y-ongfire <lweyine.sm@gmail.com>
2022-04-15 13:32:04 +0900

    head_image 및 attached_file 없을때 템플릿 개선, 다운로드 버튼 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -16,4 +17,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.attached_file.name)
 
